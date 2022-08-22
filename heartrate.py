efad1a0c35390d9af3cafdbfa5b1b6c1206640c8
yuvasreeramesh <74259341+yuvasreeramesh@users.noreply.github.com>
2022-08-22 19:33:38 +0530

    Create heartrate.py

---
+++ heartrate.py
@@ -0,0 +1,10 @@
+heart_rate=int(input("enter the heart rate:"))
+if(heart_rate<60): {
+    print("heart rate is low")
+}
+elif(heart_rate >100):{
+    print("heart rate is too high)")
+}
+else :{
+    print("heart rate is normal")
+}
